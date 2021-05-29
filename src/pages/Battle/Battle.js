6986c880a2686ecb87f29d58355256c4b8846bcb
nikonenkoincodeit <test@test.com>
2021-05-29 10:02:54 +0300

    added all pages

--- src/pages/Battle/Battle.js
+++ src/pages/Battle/Battle.js
@@ -1,12 +1,65 @@
 import Header from "../../components/Header";
 import "./Battle.css";
+import imgH from "../../assets/images/1618560631_9.jpg";
+import imgH2 from "../../assets/images/1618560680_8.jpg";
 
 export default function Battle() {
+  const getIdHamstrar = (id) => console.log(id);
   return (
     <div className="page-wrapper">
       <Header />
       <div className="container">
-        <h3>44444</h3>
+        <div className="box-battle">
+          <div className="hamstrar-cart" onClick={() => getIdHamstrar(1)}>
+            <img src={imgH} alt="" />
+            <div className="overlay">
+              <p>
+                name: <span>piter pan</span>
+              </p>
+              <p>
+                age: <span>6</span>
+              </p>
+              <p>
+                favorit food: <span>kitchen</span>
+              </p>
+              <p>
+                favorit active: <span>lego</span>
+              </p>
+              <div>
+                <h3 className="hamstrar-cart-title">Winner!</h3>
+                <p>Winner 75 procent</p>
+                <p>looser 25 procent</p>
+              </div>
+            </div>
+          </div>
+
+          <h1 className="hamstrar-title">
+            click on the picture to choose the winner
+          </h1>
+
+          <div className="hamstrar-cart" onClick={() => getIdHamstrar(2)}>
+            <img src={imgH2} alt="" />
+            <div className="overlay">
+              <p>
+                name: <span>piter pan</span>
+              </p>
+              <p>
+                age: <span>6</span>
+              </p>
+              <p>
+                favorit food: <span>kitchen</span>
+              </p>
+              <p>
+                favorit active: <span>lego</span>
+              </p>
+              <div>
+                <h3 className="hamstrar-cart-title">Winner!</h3>
+                <p>Winner 75 procent</p>
+                <p>looser 25 procent</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,12 +1,50 @@
+import { useState } from "react";
+
+import { v4 as uuidv4 } from "uuid";
 import Header from "../../components/Header";
 import "./Gallery.css";
 
+import imgH2 from "../../assets/images/1618560680_8.jpg";
+
 export default function Gallery() {
+  const array = [1, 2, 3, 1, 1, 1, 1, 1, 1];
+
+  let [open, setOpen] = useState(-1);
+
+  const deleteHamstrar = (id) => {
+    console.log(id);
+    setOpen((open = id));
+  };
+
   return (
     <div className="page-wrapper">
       <Header />
       <div className="container">
-        <h3>Gallery</h3>
+        <div className="box-gallery">
+          {array.map((item, index) => {
+            return (
+              <div className="gallery-cart" key={uuidv4()}>
+                <div
+                  className="btn-delete"
+                  onClick={() => deleteHamstrar(index)}
+                >
+                  &#10006;
+                </div>
+                <img src={imgH2} alt="" />
+                {index == open && (
+                  <div className="overla">
+                    <p>
+                      name: <span>piter pan</span>
+                    </p>
+                    <p>
+                      age: <span>6</span>
+                    </p>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
